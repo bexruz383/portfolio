7e072809dc6bfaa44405a54ef3c8846441d18fd9
bexruz383 <behruzabduqodirov84@gmail.com>
2025-06-04 10:15:55 +0300

    10:15 settings.py

--- root/settings.py
+++ root/settings.py
@@ -107,9 +107,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'root/static')]  # `root` ni loyihangiz nomiga moslang
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
