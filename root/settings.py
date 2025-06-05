b45935be13be18c56750a6d1889cb645a9903328
bexruz383 <behruzabduqodirov84@gmail.com>
2025-06-05 16:49:29 +0300

    Update settings.py

--- root/settings.py
+++ root/settings.py
@@ -14,8 +14,11 @@ SECRET_KEY = 'django-insecure-md8=ihxw@!mesl*lvfec&=4*2582q#1hj5bqe_*xt+mv_p&0s+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['https://portfoliyo-1kjs.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'portfoliyo-1kjs.onrender.com',
+]
 
 
 # Application definition
